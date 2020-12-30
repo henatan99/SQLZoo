40cb98f864a313b1f3261078d6d0940ce1ed8fcb
Henok <henatan99@gmail.com>
2020-12-30 06:23:32 +0300

    Solves SELECT within SELECT Tutorial questions

--- sqlzoo.sql
+++ sqlzoo.sql
@@ -102,4 +102,53 @@ SELECT winner, yr, subject FROM nobel
 --    Question 14
 SELECT winner, subject FROM nobel
   WHERE yr=1984
-   ORDER BY subject IN ('Physics','Chemistry'), subject,winner
+   ORDER BY subject IN ('Physics','Chemistry'), subject,winner;
+
+-- SELECT within SELECT Tutorial
+
+-- Question 1
+SELECT name FROM world
+  WHERE population >
+     (SELECT population FROM world 
+      WHERE name='Russia');
+-- Question 2
+SELECT name FROM world 
+  WHERE continent = 'Europe' 
+    AND gdp/population > (SELECT gdp/population 
+      FROM world WHERE name = 'United Kingdom');
+-- Question 3
+SELECT name, continent FROM world 
+  WHERE continent IN (SELECT continent FROM world 
+   WHERE name IN ('Argentina', 'Australia'))
+     ORDER BY name;
+-- Question 4
+SELECT name, population FROM world 
+  WHERE population > (SELECT population FROM world 
+   WHERE name = 'Canada') AND population < (SELECT population FROM world 
+   WHERE name = 'Poland');
+-- Question 5
+SELECT name, CONCAT(ROUND(population/(SELECT population FROM world 
+  WHERE name = 'Germany')*100), '%') FROM world
+    WHERE continent = 'Europe';
+-- Question 6
+SELECT name FROM world 
+  WHERE gdp > ALL(SELECT gdp FROM world
+    WHERE continent = 'Europe' AND gdp > 0);
+-- Question 7
+SELECT continent, name, area FROM world x
+  WHERE area >= ALL
+    (SELECT area FROM world y
+        WHERE y.continent=x.continent
+          AND area>0)
+-- Question 8
+SELECT continent, name FROM world x
+  WHERE name <= ALL(SELECT name FROM world y
+    WHERE y.continent = x.continent);
+-- Question 9
+SELECT name, continent, population FROM world x
+  WHERE 25000000 > ALL(SELECT population FROM world y
+    WHERE y.continent = x.continent);
+-- Question 10
+SELECT name, continent FROM world x
+  WHERE population/3 > ALL(SELECT population FROM world y
+    WHERE y.continent = x.continent AND population > 0 AND y.name != x.name); 
