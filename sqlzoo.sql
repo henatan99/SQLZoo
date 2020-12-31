17c8c7311183dade87d14b11f760f334c0e23ba7
Henok <henatan99@gmail.com>
2020-12-31 14:22:53 +0300

    Solves 'More JOIN operations' questions

--- sqlzoo.sql
+++ sqlzoo.sql
@@ -246,4 +246,83 @@ SELECT game.mdate, game.team1,
       GROUP BY mdate,  game.id, team1, team2 
         ORDER BY mdate, matchid, team1, team2;
 
+-- More JOIN operations
+
+-- Question 1
+SELECT id, title
+ FROM movie
+ WHERE yr=1962
+-- Question 2
+SELECT yr FROM movie
+  WHERE title = 'Citizen Kane';
+-- Question 3
+SELECT id, title, yr FROM movie 
+  WHERE title LIKE '%Star Trek%'
+    ORDER BY yr;
+-- Question 4
+SELECT id FROM actor 
+  WHERE name = 'Glenn Close';
+-- Question 5
+SELECT id FROM movie
+  WHERE title = 'Casablanca';
+-- Question 6
+SELECT name FROM actor 
+  JOIN casting ON actorid = id  
+   WHERE movieid = 11768;
+-- Question 7
+SELECT name FROM actor
+  JOIN casting ON actorid = id
+  JOIN movie ON movie.id = movieid 
+      WHERE title = 'Alien';
+-- Question 8
+SELECT title FROM movie 
+  JOIN casting ON movieid = movie.id
+  JOIN actor ON actorid = actor.id
+    WHERE name = 'Harrison Ford';
+-- Question 9
+SELECT title FROM movie 
+  JOIN casting ON movieid = movie.id
+  JOIN actor ON actorid = actor.id
+    WHERE name = 'Harrison Ford' AND ord != 1;
+-- Question 10
+SELECT title, name FROM movie 
+  JOIN casting ON movieid = movie.id
+  JOIN actor ON actorid = actor.id
+    WHERE yr = 1962 AND ord = 1;
+-- Question 11
+SELECT yr,COUNT(title) FROM
+  movie JOIN casting ON movie.id=movieid
+        JOIN actor   ON actorid=actor.id
+WHERE name='Rock Hudson'
+GROUP BY yr
+HAVING COUNT(title) > 2;
+-- Question 12
+SELECT title, name FROM movie
+  JOIN casting ON movie.id=movieid
+        JOIN actor ON actorid=actor.id
+WHERE movieid IN (SELECT movieid FROM casting
+  JOIN movie ON movieid = movie.id 
+  JOIN actor ON actorid = actor.id 
+    WHERE actor.name = 'Julie Andrews') AND ord = 1;
+-- Question 13
+SELECT actor.name FROM actor
+  JOIN casting ON actorid = actor.id
+    WHERE ord = 1
+      GROUP BY actor.name HAVING COUNT(*) >= 15;
+-- Question 14
+SELECT title, COUNT(*) FROM movie
+  JOIN casting ON movie.id = casting.movieid
+    WHERE movie.yr = 1978
+     GROUP BY movie.id, title
+       ORDER BY COUNT(*) DESC, title;
+-- Question 15
+SELECT a.name
+  FROM (SELECT movie.* FROM movie
+     JOIN casting ON casting.movieid = movie.id
+     JOIN actor ON actor.id = casting.actorid
+        WHERE actor.name = 'Art Garfunkel') AS m
+     JOIN (SELECT actor.*, casting.movieid FROM actor
+     JOIN casting ON casting.actorid = actor.id
+         WHERE actor.name != 'Art Garfunkel') as a ON m.id = a.movieid;
+
 
